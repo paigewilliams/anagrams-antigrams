6f3210ffc9ff641f6714fce5bfe436f31874c2a9
Paige Williams <paw145@humboldt.edu>
2018-11-16 14:43:09 -0800

    Refactor code to separate methods.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,7 +5,7 @@ class Words
     @word2 = word2
   end
 
-  def anagram
+  def real_word
     @vowels1 = @word1.scan(/[aeiou]/)
     @vowels2 = @word2.scan(/[aeiou]/)
     @word1_clean = @word1.downcase.gsub(/[^a-z]/i, '')
@@ -13,28 +13,35 @@ class Words
 
     if @vowels1 == [] ||  @vowels2 == []
       "Please enter an actual word!"
-
-    elsif @word1_clean.length == @word2_clean.length
-      @word1_array = @word1_clean.split("")
-      @word2_array = @word2_clean.split("")
-        if @word1_array.any? {|x| @word2_array.include?(x)}
-          if @word1_array.sort == @word2_array.sort
-          "These words are anagrams."
-          else
-          "These words are not anagrams."
-          end
-        else
-          "These words have no letter matches and are antigrams."
-        end
-    end
-
-  end
-  def palindrome
-    @word1_reverse = @word1.reverse()
-    @word2_reverse = @word2.reverse()
-
-    if @word1_reverse == @word1 || @word2_reverse = @word2
-      "You entered a palindrome!"
+    else
+      anagram()
     end
   end  
+
+  def anagram
+    if @word1_clean.length == @word2_clean.length
+      @word1_array = @word1_clean.split("")
+      @word2_array = @word2_clean.split("")
+      if @word1_array.any? {|x| @word2_array.include?(x)}
+        if @word1_array.sort == @word2_array.sort
+        "These words are anagrams."
+        else
+        "These words are not anagrams."
+        end
+      else
+        "These words have no letter matches and are antigrams."
+      end
+    else
+      "These words are not anagrams."
+    end
+  end
+
+  def palindrome()
+    @word1_reverse = @word1.reverse()
+    @word2_reverse = @word2.reverse()
+    if @word1_reverse == @word1 || @word2_reverse == @word2
+      "You entered a palindrome!"
+
+    end
+  end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,37 +4,42 @@ require('anagram')
 
 describe(".Words") do
 
-  it ("Finds if two words contain the same letters")do
+  it ("Finds whether two words contain the same letters")do
     words = Words.new("cinema", "iceman")
-    expect(words.anagram()).to(eq("These words are anagrams."))
+    expect(words.real_word()).to(eq("These words are anagrams."))
   end
 
-  it ("Finds if two words do not contain the same letters")do
+  it ("Finds whether two words contain the same letters")do
+    words = Words.new("cinema", "poop")
+    expect(words.real_word()).to(eq("These words are not anagrams."))
+  end
+
+  it ("Finds whether two words do not contain the same letters")do
     words = Words.new("cinema", "silent")
-    expect(words.anagram()).to(eq("These words are not anagrams."))
+    expect(words.real_word()).to(eq("These words are not anagrams."))
   end
 
-  it ("Finds if two words with different cases have the same contents and length")do
+  it ("Finds whether two words with different cases have the same contents and length")do
     words = Words.new("cinema", "Iceman")
-    expect(words.anagram()).to(eq("These words are anagrams."))
+    expect(words.real_word()).to(eq("These words are anagrams."))
   end
 
-  it ("Finds if an inputted word is a word, which means it contains vowels")do
+  it ("Finds whether an inputted word is a word, which means it contains vowels")do
     words = Words.new("trhg", "ruby")
-    expect(words.anagram()).to(eq("Please enter an actual word!"))
+    expect(words.real_word()).to(eq("Please enter an actual word!"))
   end
 
-  it ("Finds if an inputted word is an antigram, meaning that none of the letters match.")do
+  it ("Finds whether an inputted word is an antigram, meaning that none of the letters match.")do
     words = Words.new("stop", "ruby")
-    expect(words.anagram()).to(eq("These words have no letter matches and are antigrams."))
+    expect(words.real_word()).to(eq("These words have no letter matches and are antigrams."))
   end
 
-  it ("Finds if an inputted phrase is an anagram, antigram or is an actual word.")do
+  it ("Finds whether an inputted phrase is an anagram, antigram or is an actual word.")do
     words = Words.new("Halley's Comet", "Shall yet come")
-    expect(words.anagram()).to(eq("These words are anagrams."))
+    expect(words.real_word()).to(eq("These words are anagrams."))
   end
 
-  it ("Finds if an inputted phrase is an palindrome.")do
+  it ("Finds whether an inputted phrase is an palindrome.")do
     words = Words.new("kayak", "ruby")
     expect(words.palindrome()).to(eq("You entered a palindrome!"))
   end
